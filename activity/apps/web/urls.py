903b92a3ca04431f911b6d14636aa9c9aa28d653
cuicab <jcarlosuicab1995@gmail.com>
2021-10-26 21:02:30 -0500

    index.

--- activity/apps/web/urls.py
+++ activity/apps/web/urls.py
@@ -1,13 +1,15 @@
 from rest_framework import routers
 from django.conf.urls import url, include
 from django.urls import path
+from .views import *
 
 app_name = 'api'
 
 router = routers.SimpleRouter()
 
 urlpatterns = [
-    path('api/', include('activity.apps.web.api.urls'))
+    path('api/', include('activity.apps.web.api.urls')),
+    path('', IndexView.as_view(), name='index'),
 ]
 
 urlpatterns = router.urls + urlpatterns
--- activity/apps/web/views.py
+++ activity/apps/web/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.generic import View
 
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class IndexView(View):
+    template_name = "index.html"
+
+    def get(self, request):
+        ctx = {}
+        return render(request, self.template_name, ctx)
